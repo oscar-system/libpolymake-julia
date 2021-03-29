2c1efd7484c55b6fd8664c2471640063639ab8e6
Lars Kastner <lars.kastner@yahoo.dk>
2021-03-29 17:33:21 +0200

    nv and ne do not depend on 0/1 based counting

--- src/type_graph.cpp
+++ src/type_graph.cpp
@@ -21,12 +21,14 @@ void add_graph(jlcxx::Module& jlpolymake)
         .apply_combination<pm::graph::Graph, jlcxx::ParameterList<pm::graph::Undirected,pm::graph::Directed>>(
             [](auto wrapped) {
         typedef typename decltype(wrapped)::type WrappedT;
-        wrapped.method("_nv", &WrappedT::nodes);
-        wrapped.method("_ne", &WrappedT::edges);
+        
+        wrapped.method("nv", &WrappedT::nodes);
+        wrapped.method("ne", &WrappedT::edges);
         wrapped.method("_inneighbors", [](WrappedT& M, int64_t i) { return pm::Set<pm::Int>(M.in_adjacent_nodes(i)); });
         wrapped.method("_outneighbors", [](WrappedT& M, int64_t i) { return pm::Set<pm::Int>(M.out_adjacent_nodes(i)); });
         wrapped.method("_has_vertex", [](WrappedT& M, int64_t i) { return M.node_exists(i); });
         wrapped.method("_has_edge", [](WrappedT& M, int64_t tail, int64_t head) { return M.edge_exists(tail, head); });
+
         wrapped.method("show_small_obj", [](WrappedT& S) {
             return show_small_object<WrappedT>(S);
         });
