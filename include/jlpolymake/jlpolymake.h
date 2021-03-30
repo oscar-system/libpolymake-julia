1df04c4b55e2ad1cab9c75ad1080a8e1d193363e
Lars Kastner <lars.kastner@yahoo.dk>
2021-03-30 12:18:48 +0200

    Debump version to patch level

--- include/jlpolymake/jlpolymake.h
+++ include/jlpolymake/jlpolymake.h
@@ -34,10 +34,9 @@
 #pragma clang diagnostic pop
 
 #define JLPOLYMAKE_VERSION_MAJOR 0
-#define JLPOLYMAKE_VERSION_MINOR 5
-// we should not use the patch version for now as this is used in the jll for the different julia versions
-// until we drop support for julia < 1.6
-#define JLPOLYMAKE_VERSION_PATCH 0
+#define JLPOLYMAKE_VERSION_MINOR 4
+// the yggdrasil patch level will be patchlevel*100+julia_version.minor
+#define JLPOLYMAKE_VERSION_PATCH 1
 
 #define __JLPOLYMAKE_STR_HELPER(x) #x
 #define __JLPOLYMAKE_STR(x) __JLPOLYMAKE_STR_HELPER(x)
