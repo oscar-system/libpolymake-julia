db60eea5e549b800b4d35a860a249fa6b7e56064
Lars Kastner <lars.kastner@yahoo.dk>
2021-08-31 22:11:31 +0200

    graph: Add constructor, _add_edge

--- src/type_graph.cpp
+++ src/type_graph.cpp
@@ -21,13 +21,16 @@ void add_graph(jlcxx::Module& jlpolymake)
         .apply_combination<pm::graph::Graph, jlcxx::ParameterList<pm::graph::Undirected,pm::graph::Directed>>(
             [](auto wrapped) {
         typedef typename decltype(wrapped)::type WrappedT;
-        
+
+        wrapped.template constructor<int64_t>();
+
         wrapped.method("nv", &WrappedT::nodes);
         wrapped.method("ne", &WrappedT::edges);
         wrapped.method("_inneighbors", [](const WrappedT& M, int64_t i) { return pm::Set<pm::Int>(M.in_adjacent_nodes(i)); });
         wrapped.method("_outneighbors", [](const WrappedT& M, int64_t i) { return pm::Set<pm::Int>(M.out_adjacent_nodes(i)); });
         wrapped.method("_has_vertex", [](const WrappedT& M, int64_t i) { return M.node_exists(i); });
         wrapped.method("_has_edge", [](const WrappedT& M, int64_t tail, int64_t head) { return M.edge_exists(tail, head); });
+        wrapped.method("_add_edge", [](WrappedT& M, int64_t tail, int64_t head) { return M.add_edge(tail, head); });
 
         wrapped.method("show_small_obj", [](WrappedT& S) {
             return show_small_object<WrappedT>(S);
