78536b517858b0d68fb98f55fc899f908a26025c
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2021-12-23 10:57:56 +0100

    Apply suggestions from code review

--- src/type_homologygroup.cpp
+++ src/type_homologygroup.cpp
@@ -19,19 +19,15 @@ void add_homologygroup(jlcxx::Module& jlpolymake)
 
             typedef typename decltype(wrapped)::type WrappedT;
 
-            wrapped.method("betti_number", [](WrappedT& a) { return a.betti_number; });
-            wrapped.method("torsion", [](WrappedT& a) { return a.torsion; });
+            wrapped.method("betti_number", [](const WrappedT& a) { return a.betti_number; });
+            wrapped.method("torsion", [](const WrappedT& a) { return a.torsion; });
             
-            wrapped.method("take",
-                           [](pm::perl::BigObject p, const std::string& s,
-                              WrappedT& M) { p.take(s) << M; });
-
             wrapped.method("show_small_obj", [](const WrappedT& S) {
                 return show_small_object<WrappedT>(S);
             });
         });
 
-    jlpolymake.method("to_homologygroup_integer", [](pm::perl::PropertyValue pv) {
+    jlpolymake.method("to_homologygroup_integer", [](const pm::perl::PropertyValue& pv) {
         return to_SmallObject<polymake::topaz::HomologyGroup<pm::Integer>>(pv);
     });
 
