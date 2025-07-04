3d308dab1b603319903a9ff52825aa2cfdddbe77
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2025-07-04 14:23:28 +0200

    bump to 0.14.0
    test with polymake 4.14

--- test-prepare.jl
+++ test-prepare.jl
@@ -2,7 +2,7 @@ using Pkg
 
 Pkg.add(name="libcxxwrap_julia_jll", version="0.14.3")
 Pkg.pin("libcxxwrap_julia_jll")
-Pkg.add(name="polymake_jll", version="400.1300.4")
+Pkg.add(name="polymake_jll", version="400.1400.0")
 
 using polymake_jll
 using libcxxwrap_julia_jll
